a238d4cdfb39d1d7f9f5224ac91b8f190c78ebe7
Felix Prünte <47022595+failllix@users.noreply.github.com>
2024-02-18 16:01:53 +0100

    Adapt script to save local client

---
+++ helperScripts/saveLocalClient.js
@@ -0,0 +1,35 @@
+import promptSync from "prompt-sync";
+const prompt = promptSync({ sigint: true });
+
+import crypto from "crypto";
+import { execSync } from "child_process";
+
+const name = prompt("Enter the clients name: ");
+const redirectUri = prompt("Enter the client's redirect URI: ");
+
+const rawClientId = crypto.getRandomValues(new Uint8Array(32));
+
+const hashedClientIdBuffer = await crypto.subtle.digest("SHA-256", rawClientId);
+
+const clientId = Array.from(new Uint8Array(hashedClientIdBuffer))
+  .map((b) => b.toString(16).padStart(2, "0"))
+  .join("");
+
+const client = {
+  name,
+  redirectUri,
+};
+
+console.log("Generated user:");
+console.log(client);
+console.log();
+console.log(`Take note of your client id: ${clientId}`);
+console.log();
+
+console.log(`Saving object with key '${clientId}': ${JSON.stringify(client)}`);
+
+execSync(
+  `wrangler kv:key put --binding CLIENT --local ${clientId} '${JSON.stringify(
+    client
+  )}'`
+);
