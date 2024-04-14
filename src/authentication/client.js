4be096ae3f4d132d059acd0f5015349d5bfeafe6
Felix Prünte <47022595+failllix@users.noreply.github.com>
2024-04-14 21:57:28 +0200

    Feature: Improve error handling of client authentication

--- src/authentication/client.js
+++ src/authentication/client.js
@@ -1,14 +1,32 @@
+import AuthenticationError from "../error/authenticationError.js";
+import logger from "../logger.js";
 import clientStorage from "../storage/client.js";
 
 const authenticateClient = async (clientId, redirectUri) => {
-    const client = await clientStorage.getClient(clientId);
+    try {
+        const client = await clientStorage.getClient(clientId);
 
-    if (client === null) {
-        throw new Error(`Could not find client with id '${clientId}'.`);
-    }
+        if (client === null) {
+            throw new AuthenticationError({
+                errorCategory: AuthenticationError.errrorCategories.INVALID_REQUEST,
+                errorDescription: `Could not find client with id '${clientId}'.`,
+            });
+        }
 
-    if (client.redirectUri !== redirectUri) {
-        throw new Error(`Redirect URI '${redirectUri}' is not valid for client with id '${clientId}'.`);
+        if (client.redirectUri !== redirectUri) {
+            throw new AuthenticationError({
+                errorCategory: AuthenticationError.errrorCategories.INVALID_REQUEST,
+                errorDescription: `Redirect URI '${redirectUri}' is not valid for client with id '${clientId}'.`,
+            });
+        }
+    } catch (error) {
+        if (error instanceof AuthenticationError) {
+            throw error;
+        }
+
+        logger.logError(error);
+
+        throw new Error("Client authentication failed with unknown error.");
     }
 };
 
--- test/unit/authentication/client.spec.js
+++ test/unit/authentication/client.spec.js
@@ -2,9 +2,11 @@ import sinon from "sinon";
 import clientAuthenticator from "../../../src/authentication/client.js";
 
 import { assert } from "chai";
+import AuthenticationError from "../../../src/error/authenticationError.js";
+import logger from "../../../src/logger.js";
 import clientStorage from "../../../src/storage/client.js";
 
-describe("User authentication", () => {
+describe.only("User authentication", () => {
     describe("authenticateUser", () => {
         it("does not throw, if client was found and supplied redirect uri matches", async () => {
             const getClientStub = sinon.stub(clientStorage, "getClient");
@@ -15,7 +17,24 @@ describe("User authentication", () => {
             await clientAuthenticator.authenticateClient("someClient", "http://localhost/valid");
         });
 
-        it("throw, if client was not found", async () => {
+        it("logs error and throws generic error if getting client throws", async () => {
+            const getClientStub = sinon.stub(clientStorage, "getClient");
+            const expectedError = Object.freeze(new Error("Client could not be found"));
+            getClientStub.withArgs("dummyClient").throws(expectedError);
+
+            const errorLogStub = sinon.stub(logger, "logError");
+
+            try {
+                await clientAuthenticator.authenticateClient("dummyClient", "http://localhost/irrelevant");
+                return Promise.reject("Test fails, because function under test never threw error");
+            } catch (error) {
+                sinon.assert.calledOnceWithExactly(errorLogStub, expectedError);
+
+                assert.deepEqual(error, new Error("Client authentication failed with unknown error."));
+            }
+        });
+
+        it("throws if client was not found", async () => {
             const getClientStub = sinon.stub(clientStorage, "getClient");
             getClientStub.withArgs("dummyClient").resolves(null);
 
@@ -23,11 +42,17 @@ describe("User authentication", () => {
                 await clientAuthenticator.authenticateClient("dummyClient", "http://localhost/irrelevant");
                 return Promise.reject("Test fails, because function under test never threw error");
             } catch (error) {
-                assert.equal(error.message, "Could not find client with id 'dummyClient'.");
+                assert.deepEqual(
+                    error,
+                    new AuthenticationError({
+                        errorCategory: AuthenticationError.errrorCategories.INVALID_REQUEST,
+                        errorDescription: "Could not find client with id 'dummyClient'.",
+                    }),
+                );
             }
         });
 
-        it("throw, if redirect uris do not match", async () => {
+        it("throws if redirect uris do not match", async () => {
             const getClientStub = sinon.stub(clientStorage, "getClient");
             getClientStub.withArgs("dummyClient").resolves({
                 redirectUri: "http://localhost/valid",
@@ -37,7 +62,13 @@ describe("User authentication", () => {
                 await clientAuthenticator.authenticateClient("dummyClient", "http://localhost/invalid");
                 return Promise.reject("Test fails, because function under test never threw error");
             } catch (error) {
-                assert.equal(error.message, "Redirect URI 'http://localhost/invalid' is not valid for client with id 'dummyClient'.");
+                assert.deepEqual(
+                    error,
+                    new AuthenticationError({
+                        errorCategory: AuthenticationError.errrorCategories.INVALID_REQUEST,
+                        errorDescription: "Redirect URI 'http://localhost/invalid' is not valid for client with id 'dummyClient'.",
+                    }),
+                );
             }
         });
     });
