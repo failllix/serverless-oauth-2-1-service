27069dac5e5b23f70d48d80583c326b7727f5d97
Felix Prünte <47022595+failllix@users.noreply.github.com>
2025-01-19 21:23:14 +0100

    Feature: Add token endpoint request handler

---
+++ src/tokenRequestHandler.js
@@ -0,0 +1,32 @@
+import AuthenticationError from "./error/authenticationError.js";
+import logger from "./logger.js";
+import { INTERNAL_SERVER_ERROR, NOT_IMPLEMENTED } from "./responses.js";
+import authCodeTokenFlow from "./tokenFlows/authCodeTokenFlow.js";
+import tokenExchangeValidator from "./validation/tokenExchangeValidator.js";
+
+async function handleTokenRequest(request) {
+    try {
+        const formData = await request.formData();
+        const validatedGrantType = tokenExchangeValidator.isValidGrantType(formData.get("grant_type"));
+
+        logger.logMessage(`Grant type: ${validatedGrantType}`);
+
+        if (validatedGrantType === "authorization_code") {
+            return await authCodeTokenFlow.exchangeAccessCodeForToken(formData);
+        }
+
+        return NOT_IMPLEMENTED();
+    } catch (failure) {
+        logger.logError(failure);
+
+        if (failure instanceof AuthenticationError) {
+            return INTERNAL_SERVER_ERROR(failure.errorDescription);
+        }
+
+        return INTERNAL_SERVER_ERROR(failure.message);
+    }
+}
+
+export default {
+    handleTokenRequest,
+};
---
+++ test/unit/tokenRequestHandler.spec.js
@@ -0,0 +1,137 @@
+import { assert } from "chai";
+import { describe } from "mocha";
+import sinon from "sinon";
+
+import AuthenticationError from "../../src/error/authenticationError.js";
+import logger from "../../src/logger.js";
+import authCodeTokenFlow from "../../src/tokenFlows/authCodeTokenFlow.js";
+import tokenRequestHandler from "../../src/tokenRequestHandler.js";
+import tokenExchangeValidator from "../../src/validation/tokenExchangeValidator.js";
+
+describe("The token request handler", () => {
+    beforeEach(() => {
+        sinon.stub(logger);
+    });
+
+    describe("success cases", () => {
+        it("should return response of auth code flow handler when 'grant_type' is 'authorization_code'", async () => {
+            sinon.stub(authCodeTokenFlow);
+            sinon.stub(tokenExchangeValidator);
+            const requestFormDataStub = sinon.stub();
+
+            const requestStub = {
+                formData: requestFormDataStub,
+            };
+
+            const formDataGetStub = sinon.stub();
+            const formDataStub = {
+                get: formDataGetStub,
+            };
+            requestFormDataStub.resolves(formDataStub);
+            formDataGetStub.returns("someGrantType");
+
+            tokenExchangeValidator.isValidGrantType.returns("authorization_code");
+
+            authCodeTokenFlow.exchangeAccessCodeForToken.resolves("expectedReturnValue");
+
+            const response = await tokenRequestHandler.handleTokenRequest(requestStub);
+            assert.equal(response, "expectedReturnValue");
+
+            sinon.assert.calledOnceWithExactly(requestFormDataStub);
+            sinon.assert.calledOnceWithExactly(formDataGetStub, "grant_type");
+            sinon.assert.calledOnceWithExactly(tokenExchangeValidator.isValidGrantType, "someGrantType");
+            sinon.assert.calledOnceWithExactly(logger.logMessage, "Grant type: authorization_code");
+            sinon.assert.calledOnceWithExactly(authCodeTokenFlow.exchangeAccessCodeForToken, formDataStub);
+        });
+    });
+
+    describe("error cases", () => {
+        it("should return 'NOT IMPLEMENTED' response when 'grant_type' does not match expected values", async () => {
+            sinon.stub(authCodeTokenFlow);
+            sinon.stub(tokenExchangeValidator);
+            const requestFormDataStub = sinon.stub();
+
+            const requestStub = {
+                formData: requestFormDataStub,
+            };
+
+            const formDataGetStub = sinon.stub();
+            const formDataStub = {
+                get: formDataGetStub,
+            };
+            requestFormDataStub.resolves(formDataStub);
+            formDataGetStub.returns("someGrantType");
+
+            tokenExchangeValidator.isValidGrantType.returns("somethingNotImplemented");
+
+            const response = await tokenRequestHandler.handleTokenRequest(requestStub);
+            assert.equal(response.status, 501);
+            assert.equal(await response.text(), "");
+
+            sinon.assert.calledOnceWithExactly(requestFormDataStub);
+            sinon.assert.calledOnceWithExactly(formDataGetStub, "grant_type");
+            sinon.assert.calledOnceWithExactly(tokenExchangeValidator.isValidGrantType, "someGrantType");
+            sinon.assert.calledOnceWithExactly(logger.logMessage, "Grant type: somethingNotImplemented");
+            sinon.assert.notCalled(authCodeTokenFlow.exchangeAccessCodeForToken);
+        });
+        it("should log error and return internal server error when getting form data from request fails", async () => {
+            sinon.stub(authCodeTokenFlow);
+            sinon.stub(tokenExchangeValidator);
+            const requestFormDataStub = sinon.stub();
+
+            const requestStub = {
+                formData: requestFormDataStub,
+            };
+
+            const expectedError = new Error("Something broke");
+            requestFormDataStub.throws(expectedError);
+
+            const response = await tokenRequestHandler.handleTokenRequest(requestStub);
+
+            assert.equal(response.status, 500);
+            assert.equal(await response.text(), "Something broke");
+
+            sinon.assert.calledOnceWithExactly(requestFormDataStub);
+            sinon.assert.calledOnceWithExactly(logger.logError, expectedError);
+
+            sinon.assert.notCalled(tokenExchangeValidator.isValidGrantType);
+            sinon.assert.notCalled(logger.logMessage);
+            sinon.assert.notCalled(authCodeTokenFlow.exchangeAccessCodeForToken);
+        });
+        it("should log error and return internal server error with error description when validating 'grant_type' throws authentication error", async () => {
+            sinon.stub(authCodeTokenFlow);
+            sinon.stub(tokenExchangeValidator);
+            const requestFormDataStub = sinon.stub();
+
+            const requestStub = {
+                formData: requestFormDataStub,
+            };
+
+            const formDataGetStub = sinon.stub();
+            const formDataStub = {
+                get: formDataGetStub,
+            };
+            requestFormDataStub.resolves(formDataStub);
+            formDataGetStub.returns("someGrantType");
+
+            const expectedError = new AuthenticationError({
+                errorCategory: "foo",
+                errorDescription: "expectedErrorResponse",
+            });
+
+            tokenExchangeValidator.isValidGrantType.throws(expectedError);
+
+            const response = await tokenRequestHandler.handleTokenRequest(requestStub);
+
+            assert.equal(response.status, 500);
+            assert.equal(await response.text(), "expectedErrorResponse");
+
+            sinon.assert.calledOnceWithExactly(requestFormDataStub);
+            sinon.assert.calledOnceWithExactly(logger.logError, expectedError);
+            sinon.assert.calledOnceWithExactly(tokenExchangeValidator.isValidGrantType, "someGrantType");
+
+            sinon.assert.notCalled(logger.logMessage);
+            sinon.assert.notCalled(authCodeTokenFlow.exchangeAccessCodeForToken);
+        });
+    });
+});
