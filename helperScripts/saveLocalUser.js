51acd76efe4237af4d238b8be94e1f7f3dd7ab64
Felix Prünte <47022595+failllix@users.noreply.github.com>
2025-03-23 17:56:15 +0100

    Feature: Ensure users are allowed to request 'userInfo' scopes

--- helperScripts/saveLocalUser.js
+++ helperScripts/saveLocalUser.js
@@ -6,41 +6,33 @@ import crypto from "crypto";
 
 const username = prompt("Enter a user- / loginname (e.g. jdoe): ");
 const name = prompt("Enter the user's full name (e.g. John Doe): ");
-const scopeString = prompt(
-  "Enter a comma separated list of scopes (e.g. 'MY_SCOPE1,MY_SCOPE2'): "
-);
+const scopeString = prompt("Enter a comma separated list of scopes (e.g. 'MY_SCOPE1,MY_SCOPE2'): ");
 const password = prompt("Enter the password: ", { echo: "*" });
 
 const encodedPassword = new TextEncoder().encode(password);
 
-const key = await crypto.subtle.importKey(
-  "raw",
-  encodedPassword,
-  { name: "PBKDF2" },
-  false,
-  ["deriveBits"]
-);
+const key = await crypto.subtle.importKey("raw", encodedPassword, { name: "PBKDF2" }, false, ["deriveBits"]);
 
 const salt = crypto.getRandomValues(new Uint8Array(16));
 
 const derivedBits = await crypto.subtle.deriveBits(
-  {
-    name: "PBKDF2",
-    salt,
-    iterations: 1000000,
-    hash: "SHA-512",
-  },
-  key,
-  512
+    {
+        name: "PBKDF2",
+        salt,
+        iterations: 1000000,
+        hash: "SHA-512",
+    },
+    key,
+    512,
 );
 
 const passwordHash = btoa(new Uint8Array(derivedBits));
 
 const user = {
-  name,
-  salt: btoa(salt),
-  passwordToken: passwordHash,
-  scope: scopeString.split(","),
+    name,
+    salt: btoa(salt),
+    passwordToken: passwordHash,
+    scope: ["userInfo", ...scopeString.split(",")],
 };
 console.log("Generated user:");
 console.log(user);
